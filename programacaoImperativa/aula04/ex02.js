5ca97ecbe9ba053388a42b885e9e3b923aa09447
Mateus Dias <imattdias@gmail.com>
2021-08-16 11:18:17 -0300

    Exercício 02

---
+++ programacaoImperativa/aula04/ex02.js
@@ -0,0 +1,8 @@
+let nome = "Mateus"; // Variável tipo string (texto)
+let idade = 27; // Variável tipo número
+let presencaAula = true; // Variável tipo boolean (lógico – true ou false)
+
+console.log("Nome......"+nome);
+console.log("Idade........."+idade);
+console.log("Aluno está presente?");
+console.log(presencaAula);

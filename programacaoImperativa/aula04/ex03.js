cfe38c7a6bb4aa9fe2381c1250ee6252c925f139
Mateus Dias <imattdias@gmail.com>
2021-08-16 11:18:26 -0300

    Exercício 03

---
+++ programacaoImperativa/aula04/ex03.js
@@ -0,0 +1,11 @@
+let x = 4;
+let y = 5;
+let z = "olá" + 1;
+
+console.log(z);
+
+let nome = "Mateus";
+let sobrenome = "Dias";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);

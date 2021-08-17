f4ab9feef1529dad367bafafb5e7f06aa5d87687
Mateus Dias <imattdias@gmail.com>
2021-08-17 14:59:17 -0300

    Exercício de Função com parâmetro

---
+++ programacaoImperativa/aula05/ex02FuncaoParametro.js
@@ -0,0 +1,10 @@
+// Aula 05 - 17.08.2021
+// Função com parâmetro
+
+function cumprimentar(nome){
+    console.log(nome);
+}
+
+cumprimentar("Roberta");
+cumprimentar("Rosana");
+cumprimentar("Fernando");

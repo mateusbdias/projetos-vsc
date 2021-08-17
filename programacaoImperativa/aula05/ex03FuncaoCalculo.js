eedbb038311e4574b8e27cbe100346bf06ae07fe
Mateus Dias <imattdias@gmail.com>
2021-08-17 15:08:52 -0300

    Exercícios de Função

---
+++ programacaoImperativa/aula05/ex03FuncaoCalculo.js
@@ -0,0 +1,14 @@
+// Aula 05 - 17.08.2021
+// Função com cálculo
+
+function somar(a, b){
+    console.log("Total.........: " + a + b);
+}
+
+somar(2, 3);
+
+function multiplicar(x, y){
+    console.log(resultado = x * y);
+}
+
+multiplicar(4, 7);
---
+++ programacaoImperativa/aula05/ex04.js
@@ -0,0 +1,9 @@
+// Aula 05 - 17.08.2021
+// Concatenação
+
+function cumprimentar(nome){
+    const mensagem = "Olá, seja bem vindo, ";
+    console.log(mensagem + nome);
+}
+
+cumprimentar("Roberta");

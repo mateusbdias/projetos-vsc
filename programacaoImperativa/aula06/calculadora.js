81803353a406d8cd19dac8a91b15e567d48f77a0
mateusbdias <imattdias@gmail.com>
2021-08-19 12:04:07 -0300

    Partes II e III - Mesa de Trabalho - Aula 06

--- programacaoImperativa/aula06/calculadora.js
+++ programacaoImperativa/aula06/calculadora.js
@@ -16,9 +16,29 @@ function dividir(a, b){
     return a / b;
 }
 
+function quadradoDoNumero(x){
+    return multiplicar(x, x);
+}
+
+function mediaDeTresNumeros(a, b, c){
+    return adicionar(adicionar(a, b), c) / 3;
+}
+
+function calculaPorcentagem(tot, pct){
+    return (pct / 100) * tot;
+}
+
+function geradorDePorcentagem(a, b){
+    return ((a / b) * 100) + "%";
+}
+
 console.log("===== Calculadora =====");
 console.log(adicionar(15, 8));
 console.log(subtrair(23, 6));
 console.log(multiplicar(14, 3));
 console.log(dividir(38, 6));
-console.log(dividir(0, 4));
+console.log(dividir(4, 0));
+console.log(quadradoDoNumero(4));
+console.log(mediaDeTresNumeros(3, 4, 5));
+console.log(calculaPorcentagem(300, 50));
+console.log(geradorDePorcentagem(38, 200));

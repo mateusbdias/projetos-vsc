2021407cc1555eca0cc7b43e8fcedf331643a2da
mateusbdias <imattdias@gmail.com>
2021-08-19 11:52:29 -0300

    Parte I - Mesa de Trabalho - Aula 06

---
+++ programacaoImperativa/aula06/calculadora.js
@@ -0,0 +1,24 @@
+// Aula 06 - Mesa de Trabalho
+
+function adicionar(a, b){
+    return a + b;
+}
+
+function subtrair(a, b){
+    return a - b;
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
+
+function dividir(a, b){
+    return a / b;
+}
+
+console.log("===== Calculadora =====");
+console.log(adicionar(15, 8));
+console.log(subtrair(23, 6));
+console.log(multiplicar(14, 3));
+console.log(dividir(38, 6));
+console.log(dividir(0, 4));

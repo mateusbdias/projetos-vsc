01373e952886e0340b18f2a6f7445670cb0109a8
mateusbdias <imattdias@gmail.com>
2021-08-23 08:50:29 -0300

    Exercício 01 de operadores lógicos

---
+++ programacaoImperativa/aula07/ex01.js
@@ -0,0 +1,15 @@
+/*
+Aula 07 - 23.08.2021
+Operadores Lógicos
+*/
+
+var tarefasFeitas = true;
+
+if (tarefasFeitas == true){
+    var granaFilhote = 10;
+} else {
+    granaFilhote = 0;
+}
+
+console.log("Tarefas Feitas?..." + tarefasFeitas);
+console.log("Grana para o filhote....." + granaFilhote)

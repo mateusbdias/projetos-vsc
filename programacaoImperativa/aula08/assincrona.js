19abf1338039cdbd8a65e3d9f5eb1afbd16d4243
mateusbdias <imattdias@gmail.com>
2021-08-23 19:47:45 -0300

    Aula 08 - exercício PG

---
+++ programacaoImperativa/aula08/assincrona.js
@@ -0,0 +1,19 @@
+// Exercício: Meu primeiro if
+
+let status = false;
+
+if (status == true) {
+    console.log("O valor é true - verdadeiro");
+} else {
+    console.log("O valor é false - falso");
+}
+
+// Exercício: Igualdade, if e else
+
+let linguagem = "html";
+
+if (linguagem == "javascript") {
+    console.log("Estou aprendendo")
+} else {
+    console.log("Aprenderei mais tarde")
+}

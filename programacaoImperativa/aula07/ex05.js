e60f77628233ac37b45aa73f9c85b5a3424b3b6c
mateusbdias <imattdias@gmail.com>
2021-08-23 09:11:30 -0300

    Exercícios de operadores lógicos

--- programacaoImperativa/aula07/ex05.js
+++ programacaoImperativa/aula07/ex05.js
@@ -1,9 +1,9 @@
-// Aula 07 - Exercício 04
+// Aula 07 - Exercício 05
 
 var n1 = 10;
-var n2 = "10";
+var n2 = 10;
 
-if(n1 === n2){
+if(n1 > n2){
     console.log("True");
 } else {
     console.log("False");

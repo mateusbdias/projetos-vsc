3ccc5e0d9b2201ba26e33952bb5c2fc257dffe9a
mateusbdias <imattdias@gmail.com>
2021-08-23 09:32:46 -0300

    Exercícios de operadores lógicos

--- programacaoImperativa/aula07/ex06.js
+++ programacaoImperativa/aula07/ex06.js
@@ -3,7 +3,17 @@
 var n1 = 10;
 var n2 = 15;
 
-if((10 > 15) && (10 != 20)){
+if ((10 > 15) && (10 != 20)) {
+    console.log("True");
+} else
+    console.log("False");
+
+if ((12 % 4 == 0) && (12 != 24)) {
+    console.log("True");
+} else
+    console.log("False");
+
+if ((10 > 15) || (10 != 20)) {
     console.log("True");
 } else
     console.log("False");

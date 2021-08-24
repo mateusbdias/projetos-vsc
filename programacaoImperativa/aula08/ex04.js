b2b3d9086af52f5dd8fc552a2dd1d0effc59eabc
mateusbdias <imattdias@gmail.com>
2021-08-24 14:52:48 -0300

    Exercícios Condicionais JS

---
+++ programacaoImperativa/aula08/ex04.js
@@ -0,0 +1,8 @@
+// Aula 08 - 24.08.2021
+// If - Else If - Else
+
+// If Ternário
+
+var oMaior = 4 > 10 ? "O 4 é o maior" : "O 10 é o maior";
+
+console.log(oMaior);
---
+++ programacaoImperativa/aula08/ex05.js
@@ -0,0 +1,15 @@
+// Aula 08 - 24.08.2021
+// Switch
+
+let idade = 11;
+
+switch (idade) {
+    case 5:
+        console.log("Tem 5 anos");
+        break;
+    case 10:
+        console.log("Tem 10 anos");
+        break;
+    default:
+        console.log("Digite 5 ou 10 anos, apenas. Tente novamente!")
+}

838644f74b02c990c53db0004cd6eb2f3abdc502
mateusbdias <imattdias@gmail.com>
2021-08-24 14:14:58 -0300

    Aula 08 - Condicionais JS

---
+++ programacaoImperativa/aula08/ex01.js
@@ -0,0 +1,15 @@
+// Aula 08 - 24.08.2021
+// If - Else If - Else
+
+// Condicional Simples
+
+var n1 = 10;
+var n2 = 20;
+
+if (n1 == 10) {
+    console.log("True");
+}
+
+if (n2 == 20) {
+    console.log("True");
+}

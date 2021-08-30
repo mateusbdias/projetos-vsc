1bc97044c3d018e3d33cbc269b5942677a7216c1
mateusbdias <imattdias@gmail.com>
2021-08-30 19:54:42 -0300

    Aula 11 - Assíncrona

--- programacaoImperativa/aula11/assincronaWhile.js
+++ programacaoImperativa/aula11/assincronaWhile.js
@@ -2,7 +2,7 @@
 // Exercícios While
 
 // Exercício 1
-/* let numero = 1;
+let numero = 1;
 function numerosImpares(numero){
     while(numero <= 100){
         if(numero % 2 != 0){
@@ -12,10 +12,10 @@ function numerosImpares(numero){
     }
 }
 
-numerosImpares(numero); */
+numerosImpares(numero);
 
 // Exercício 2
-/* let numero = 1;
+let numero = 1;
 function numerosPares(numero){
     while(numero <= 100){
         if(numero % 2 == 0){
@@ -25,27 +25,41 @@ function numerosPares(numero){
     }
 }
 
-numerosPares(numero); */
+numerosPares(numero);
 
 // Exercício 3
 let i = 1;
 let pares = [];
 let impares = [];
-
 function separarParesImpares(numero){
     while(i <= numero){
-        if(numero % 2 == 0){
+        if(i % 2 == 0){
             pares.push(i);
-            console.log(pares);
         }
         else{
             impares.push(i);
-            console.log(impares);
         }
         i++
     }
+    console.log("Números pares:");
+    console.log(pares);
+    console.log("==============");
+    console.log("Números ímpares:");
+    console.log(impares);
 }
 
-separarParesImpares(6);
+separarParesImpares(40);
 
 // Exercício 4
+let i = 0;
+let soma = 0;
+function mediaNotas(alunos, notas){
+    while(i < alunos){
+        soma = soma + notas[i];
+    i++
+    }
+    console.log("Média das notas:");
+    console.log(soma / alunos);
+}
+
+mediaNotas(5, [5, 6, 7, 8, 9]);

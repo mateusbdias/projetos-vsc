33d6b49bc2b9574fbcb46b6d63a7fb09151e903f
mateusbdias <imattdias@gmail.com>
2021-08-31 08:48:26 -0300

    Aula 10 - Exercícios adequados com For e While

--- programacaoImperativa/aula10/mesaTrabalho.js
+++ programacaoImperativa/aula10/mesaTrabalho.js
@@ -4,7 +4,7 @@
 
 // PARTE 1
 // Exercício 1
-let numbers = [22, 33, 54, 66, 72];
+/* let numbers = [22, 33, 54, 66, 72];
 console.log(numbers[numbers.length]);
 // Retorna undefined
 
@@ -16,40 +16,40 @@ console.log(grupoDeAmigos[1][0]);
 // Exercício 3
 let str = "una string qualquer";
 let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ];
-console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]);
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]); */
 // Retorna "una string qualquer"
 
 // PARTE 2 - Array Invertido
-// Exercício 1 - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
-function imprimirInverso(array){
-    console.log(array.pop());
-    console.log(array.pop());
-    console.log(array.pop());
+// Exercício 1
+/* function imprimirInverso(array){
+    for (i = 1; i <= array.length; i++){
+        console.log(array[array.length - i]);
+    }
 }
 
-imprimirInverso([12, 23, 34]);
+imprimirInverso([12, 23, 34, 45]); */
 
 // Exercício 2 - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
+/* let novoArray = [];
 function inverter(array){
-    let novoArray = [];
-    novoArray.push(array.pop());
-    novoArray.push(array.pop());
-    novoArray.push(array.pop());
+    for (i = 1; i <= array.length; i++){
+        novoArray.push(array[array.length - i]);
+    }
     console.log(novoArray);
 }
 
-inverter([1, 2, 3]);
+inverter([1, 2, 3]); */
 
 // PARTE 3 - somarArray() - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
-let soma = 0;
+/* let soma = 0;
 function somarArray(array){
-    soma = soma + array[0];
-    soma = soma + array[1];
-    soma = soma + array[2];
+    for (i = 0; i < array.length; i++){
+        soma = soma + array[i]
+    }
     console.log(soma);
 }
 
-somarArray([2, 3, 5]);
+somarArray([2, 3, 5]); */
 
 // PARTE 4 - Simulação Array.join() - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
 function join(array){
@@ -60,7 +60,7 @@ join(["o", "l", "á"]);
 
 // PARTE 5 - Coleções de Filmes (e mais...) - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
 // Exercício 1
-let filmes = [];
+/* let filmes = [];
 filmes.push("star wars");
 filmes.push("matrix");
 filmes.push("mr. robot");
@@ -90,4 +90,4 @@ console.log(filmesAnimacao);
 
 function unirArray(array1, array2){
     
-}
+} */

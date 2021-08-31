505c6b890d0a6744cb76226a523e2692421c8446
mateusbdias <imattdias@gmail.com>
2021-08-31 11:03:38 -0300

    Aula 10 - Mesa de Trabalho - Complementada com For

--- programacaoImperativa/aula10/mesaTrabalho.js
+++ programacaoImperativa/aula10/mesaTrabalho.js
@@ -1,10 +1,10 @@
 // Mesa de Trabalho 1
-// Aula 10 - 30-08-2021
-// Integrantes: Alexandre Pedro, Phillipe Borges, Wesley Bueno, Mateus Benites Dias
+// Aula 10 - 30.08.2021
+// Mateus Benites Dias
 
 // PARTE 1
 // Exercício 1
-/* let numbers = [22, 33, 54, 66, 72];
+let numbers = [22, 33, 54, 66, 72];
 console.log(numbers[numbers.length]);
 // Retorna undefined
 
@@ -16,21 +16,21 @@ console.log(grupoDeAmigos[1][0]);
 // Exercício 3
 let str = "una string qualquer";
 let grupoDeAmigos = [ [45, 89, 0], ["Digital", "House", true], ["string", "123","false", 54, true, str] ];
-console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]); */
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]);
 // Retorna "una string qualquer"
 
 // PARTE 2 - Array Invertido
 // Exercício 1
-/* function imprimirInverso(array){
+function imprimirInverso(array){
     for (i = 1; i <= array.length; i++){
         console.log(array[array.length - i]);
     }
 }
 
-imprimirInverso([12, 23, 34, 45]); */
+imprimirInverso([12, 23, 34, 45]);
 
 // Exercício 2 - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
-/* let novoArray = [];
+let novoArray = [];
 function inverter(array){
     for (i = 1; i <= array.length; i++){
         novoArray.push(array[array.length - i]);
@@ -38,10 +38,10 @@ function inverter(array){
     console.log(novoArray);
 }
 
-inverter([1, 2, 3]); */
+inverter([1, 2, 3]);
 
 // PARTE 3 - somarArray() - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
-/* let soma = 0;
+let soma = 0;
 function somarArray(array){
     for (i = 0; i < array.length; i++){
         soma = soma + array[i]
@@ -49,45 +49,77 @@ function somarArray(array){
     console.log(soma);
 }
 
-somarArray([2, 3, 5]); */
+somarArray([2, 3, 5]);
 
 // PARTE 4 - Simulação Array.join() - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
+let texto = "";
 function join(array){
-    console.log(array[0] + array[1] + array[2]);
+    for(i = 0; i < array.length; i++){
+        texto = texto + array[i];
+    }
+    console.log(texto);
 }
 
 join(["o", "l", "á"]);
 
 // PARTE 5 - Coleções de Filmes (e mais...) - NÃO FOI POSSÍVEL ADEQUAR A QUALQUER ARRAY - CONTEÚDO PENDENTE
 // Exercício 1
-/* let filmes = [];
-filmes.push("star wars");
-filmes.push("matrix");
-filmes.push("mr. robot");
-filmes.push("o preço do amanhã");
-filmes.push("a vida é bela");
+let filmes = [];
+filmes.push("star wars", "matrix", "mr. robot", "o preço do amanhã", "a vida é bela");
 console.log(filmes);
 
 // Exercício 2
+let filmesCaps = [];
 function maiusculas(array){
-    array[0].toUpperCase();
-    array[1].toUpperCase();
-    array[2].toUpperCase();
-    array[3].toUpperCase();
-    array[4].toUpperCase();
+    for(i = 0; i < array.length; i++){
+        filmesCaps.push(array[i].toUpperCase());
+    }
+    console.log(filmesCaps);
 }
 
-console.log(maiusculas(filmes));
+maiusculas(filmes);
 
 // Exercício 3
 let filmesAnimacao = [];
-filmesAnimacao.push("toy story");
-filmesAnimacao.push("finding Nemo");
-filmesAnimacao.push("kung-fu panda");
-filmesAnimacao.push("wally");
-filmesAnimacao.push("fortnite");
+filmesAnimacao.push("toy story", "finding Nemo", "kung-fu panda", "wally", "fortnite");
 console.log(filmesAnimacao);
 
 function unirArray(array1, array2){
-    
-} */
+    for(i = 0; i < array2.length; i++){
+        array1.push(array2[i]);
+    }
+    console.log(array1);
+}
+
+unirArray(filmes, filmesAnimacao);
+
+// Exercício 4
+let filmesAnimacao = [];
+let ultimoElemento = [];
+filmesAnimacao.push("toy story", "finding Nemo", "kung-fu panda", "wally", "fortnite");
+console.log(filmesAnimacao);
+
+function unirArray(array1, array2){
+    ultimoElemento.push(array2.pop());
+    for(i = 0; i < array2.length; i++){
+        array1.push(array2[i]);
+    }
+    console.log(array1);
+}
+
+unirArray(filmes, filmesAnimacao);
+
+// Exercício 5
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+function comparaNotas(notas1, notas2){
+    for(i = 0; i < notas1.length; i++){
+        if(notas1[i] == notas2[i]){
+            console.log(i + ". As notas são iguais");
+        } else {
+            console.log(i + ". As notas são diferentes");
+        }
+    }
+}
+
+comparaNotas(asiaScores, euroScores);

b996c82e16e5a0ca5ee952cc31b5119493286b4d
mateusbdias <imattdias@gmail.com>
2021-09-02 11:29:40 -0300

    progImp - Aula 12 - Exercício Slice

---
+++ programacaoImperativa/aula12/ex01Slice.js
@@ -0,0 +1,9 @@
+// Aula 12 - 02.09.2021
+// Slice - método que extrai um trecho da string
+
+let frase = "Breaking Bad Rules!";
+let a = frase.slice(0);
+let b = frase.slice(0,8);
+
+console.log(a);
+console.log(b);

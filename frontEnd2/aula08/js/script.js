13bdd89aa2a0a0904887bbc28b56d46b10821405
Mateus Benites Dias <imattdias@gmail.com>
2021-11-04 15:21:20 -0300

    frontEnd - Aula 08

---
+++ frontEnd2/aula08/js/script.js
@@ -0,0 +1,10 @@
+let submitBtn = document.querySelector('#submit');
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+// window.onload = () => {
+//     alert('Página carregada');
+// }
+

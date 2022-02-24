6561efdb7cfafab6846e7cc14786d0058b3c44c5
Mateus Benites Dias <imattdias@gmail.com>
2022-02-24 15:44:46 -0300

    CTD - Front End III - Aula 03 - Exercício Sync

--- frontEnd3/aula03S/my-app/src/App.js
+++ frontEnd3/aula03S/my-app/src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
 
 function App() {
+  const [valor, setValor] = useState(0);
+  
+  function aumentarValor(){
+    setValor(valor + 1);
+  }
+  
+  function reduzirValor(){
+    setValor(valor - 1);
+  }
+
   return (
     <>
       <h1>Hello World!</h1>
+      <h2>{valor}</h2>
+      <button onClick={aumentarValor}>Aumentar</button>
+      <button onClick={reduzirValor}>Reduzir</button>
     </>
   );
 }

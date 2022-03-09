89a268ec0045bef4791a2ced85b434957a6e0d6a
Mateus Benites Dias <imattdias@gmail.com>
2022-03-09 18:53:05 -0300

    CTD - Front End III - Aula 04 - Mesa de Trabalho

---
+++ frontEnd3/aula04S/src/App.js
@@ -0,0 +1,12 @@
+import imagem from './assets/homem-feliz-dinheiro.jpeg'
+
+function App() {
+  return (
+    <>
+      <h1>Projeto Limpo!</h1>
+      <img src={imagem} alt="homem feliz" />
+    </>
+  );
+}
+
+export default App;

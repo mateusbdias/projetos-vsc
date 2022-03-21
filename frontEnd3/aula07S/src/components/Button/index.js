edbf2d28453985b544625f28d6145e26e5dd365f
Mateus Benites Dias <imattdias@gmail.com>
2022-03-21 17:56:55 -0300

    CTD - Front End III - Aula 07 - Moved

--- frontEnd3/aula07S/src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export default function Button(props){
-    return(
-        <button 
-        className={props.texto === "Adicionar" ? "button" : "button2"}
-        >{props.texto}</button>
-    );
-}
